239b65e6ab2e10ceb0a2a9686af4eff7f890e633
Alexander Pelta <alexpxrad@gmail.com>
2022-08-15 10:19:07 -0400

    first login form with antd

---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import { Form, Input, Button} from 'antd'
+
+
+
+function Login() {
+    const handleForm = ({ email.password }) => {
+        alert('Form submitted with ' + email + ' and ' +password)
+
+    }
+    return (
+        <>
+        <h2>Login</h2>
+        <Form onFinish={handleForm} labelCol= {{ span: 8} wrapperCol= {{ span: 16}}}>
+            <Form.Item label= "Email" name="email" rules = {[{
+                required: true,
+                message: 'Please enter a vaild email'
+            
+            }]}>
+                <Input/>
+                <Form.Item>
+                <Form.Item label= "Password" name="password" rules = {[{
+                required: true,
+                message: 'Please enter a vaild email'
+            }]}>
+            <Input.Password/>
+            </Form.Item>
+            <Button type='primary' htmlType='submit' >Submit</Button>
+        <Form/>
+        </>
+
+    )
+
+
+
+}
+
+export default Login; 
